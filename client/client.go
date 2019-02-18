df7c4171f40293ad35e993fcef7b704c555f9a0f
kAldown <kaldownb@gmail.com>
2019-02-18 17:18:19 +0300

    Added [server] [client]

---
+++ client/client.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	pb "../proto"
+	"context"
+	"google.golang.org/grpc"
+	"log"
+	"time"
+)
+
+const (
+	address     = "localhost:5001"
+	defaultName = "salo"
+)
+
+func main() {
+	conn, err := grpc.Dial(address, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	c := pb.NewQuizerClient(conn)
+
+	quiz := defaultName
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	r, err := c.GetQuiz(ctx, &pb.QuizRequest{Message: quiz})
+	if err != nil {
+		log.Printf("GetQuiz Request failed: %v", err)
+
+		// TEST: try to fillup and ask again
+		r2, err2 := c.SetQuiz(ctx, &pb.QuizRequest{Message: quiz})
+		if err2 != nil {
+			log.Fatalf("SetQuiz Request failed: %v", err2)
+		}
+		log.Printf("Response: %v", r2.Message)
+
+		// ask again
+		r3, err3 := c.GetQuiz(ctx, &pb.QuizRequest{Message: quiz})
+		if err3 != nil {
+			log.Fatalf("GetQuiz Second Request failed: %v", err3)
+		}
+		log.Printf("Response: %v", r3.Message)
+		return
+		// END Test
+
+	}
+	log.Printf("Response: %v", r.Message)
+}
---
+++ server/server.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	pb "../build/generated"
+	"context"
+	"errors"
+	"google.golang.org/grpc"
+	"log"
+	"net"
+)
+
+const (
+	port = ":5001"
+)
+
+var quizes = map[string]string{}
+
+type server struct{}
+
+func (s *server) GetQuiz(ctx context.Context, in *pb.QuizRequest) (*pb.QuizResponse, error) {
+	quiz, ok := quizes[in.Message]
+	if !ok {
+		return nil, errors.New("No such quiz")
+	}
+	return &pb.QuizResponse{Message: quiz}, nil
+}
+
+func (s *server) SetQuiz(ctx context.Context, in *pb.QuizRequest) (*pb.QuizResponse, error) {
+	quizes[in.Message] = in.Message
+	return &pb.QuizResponse{Message: "Quiz was saved"}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterQuizerServer(s, &server{})
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
